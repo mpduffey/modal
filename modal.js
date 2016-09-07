9847b207e8180aba2abfca305828cd6132d30a7d
Mike Duffey <mpduffey@gmail.com>
2016-09-07 17:40:46 +0000

    wwft update

--- modal.js
+++ modal.js
@@ -9,17 +9,36 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var modal_service_1 = require('modules/modal/modal-service');
 var Modal = (function () {
-    function Modal() {
+    function Modal(modal) {
+        var _this = this;
+        this.modal = modal;
+        this.showFooter = false;
+        this.title = "Modal Title";
+        this.closeModal = function () {
+            _this.modal.showModal = false;
+        };
+        this.modal = modal;
     }
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], Modal.prototype, "showFooter", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], Modal.prototype, "title", void 0);
     Modal = __decorate([
         core_1.Component({
             selector: 'modal',
-            template: "\n\t\t<div class=\"modal fade\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\">\n\t\t\t<div class=\"modal-dialog\" role=\"document\">\n\t\t\t\t<div class=\"modal-content\">\n\t\t\t\t\t<div class=\"modal-header\">\n\t\t\t\t\t\t<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n\t\t\t\t\t\t<h4 class=\"modal-title\">Modal title</h4>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"modal-body\">\n\t\t\t\t\t\t...\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"modal-footer\">\n\t\t\t\t\t\t<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>\n\t\t\t\t\t\t<button type=\"button\" class=\"btn btn-primary\">Save changes</button>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n\t"
+            template: "\n\t\t<div *ngIf=\"modal.showModal\" class=\"modal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\">\n\t\t\t<div class=\"modal-dialog\" role=\"document\">\n\t\t\t\t<div class=\"modal-content\">\n\t\t\t\t\t<div class=\"modal-header\">\n\t\t\t\t\t\t<button type=\"button\" class=\"close\" (click)=\"modal.showModal=false;\"><span aria-hidden=\"true\">&times;</span></button>\n\t\t\t\t\t\t<h4 class=\"modal-title\">{{modal.title}}</h4>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"modal-body\">\n\t\t\t\t\t\t<reactive-form [formObject]=\"modal.formObject.formObject\" [fieldObject]=\"modal.formObject.fieldObject\" [cancelFunction]=\"closeModal\"></reactive-form>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div *ngIf=\"showFooter\" class=\"modal-footer\">\n\t\t\t\t\t\t<button type=\"button\" class=\"btn btn-default\" (click)=\"modal.showModal=false;\">Close</button>\n\t\t\t\t\t\t<button type=\"button\" class=\"btn btn-primary\">Save changes</button>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n\t",
+            styles: ["\n\t\t.modal {display: block;}\n\t"]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [(typeof (_a = typeof modal_service_1.ModalService !== 'undefined' && modal_service_1.ModalService) === 'function' && _a) || Object])
     ], Modal);
     return Modal;
+    var _a;
 }());
 exports.Modal = Modal;
 //# sourceMappingURL=modal.js.map
--- modal.ts
+++ modal.ts
@@ -1,28 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, Input}			from '@angular/core';
+import {ModalService}         from 'modules/modal/modal-service';
 
 @Component({
 	selector:				'modal',
 	template:				`
-		<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div *ngIf="modal.showModal" class="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<button type="button" class="close" (click)="modal.showModal=false;"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title">{{modal.title}}</h4>
 					</div>
 					<div class="modal-body">
-						...
+						<reactive-form [formObject]="modal.formObject.formObject" [fieldObject]="modal.formObject.fieldObject" [cancelFunction]="closeModal"></reactive-form>
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<div *ngIf="showFooter" class="modal-footer">
+						<button type="button" class="btn btn-default" (click)="modal.showModal=false;">Close</button>
 						<button type="button" class="btn btn-primary">Save changes</button>
 					</div>
 				</div>
 			</div>
 		</div>
-	`
+	`,
+	styles:				[`
+		.modal {display: block;}
+	`]
 })
 
 export class Modal {
-	
+	@Input() showFooter = false;
+	@Input() title = "Modal Title";
+
+	constructor(private modal: ModalService) {
+		this.modal = modal;
+	}
+	closeModal = () => {
+		this.modal.showModal = false;
+	}
 }
